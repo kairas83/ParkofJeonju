1a0454c277438d8934c6c973713b60c3a338186a
WonHo Bark <kairas@outlook.kr>
2016-11-08 21:19:11 +0900

    change name of park image to english from korean

---
+++ app/src/main/java/org/iptime/kairas/parkofjeonju/park_list.java
@@ -0,0 +1,11 @@
+package org.iptime.kairas.parkofjeonju;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class park_list extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parklist);
+    }
+}
