e34d7bea18fed4439b762f610aea05182bd1edfb
WonHo Bark <kairas@outlook.kr>
2016-11-09 20:25:50 +0900

    add list view
    리스트뷰 기본적인 구조 작성

--- app/src/main/java/org/iptime/kairas/parkofjeonju/ParkListActivity.java
+++ app/src/main/java/org/iptime/kairas/parkofjeonju/ParkListActivity.java
@@ -2,10 +2,36 @@ package org.iptime.kairas.parkofjeonju;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ParkListActivity extends AppCompatActivity {
+    ArrayList<MyItem> arItem;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parklist);
+        setTitle("공원 목록");
+
+        arItem = new ArrayList<MyItem>();
+        MyItem mi;
+        mi = new MyItem(R.drawable.chamseam_park, "참새암공원");
+        arItem.add(mi);
+
+        MyListAdapter myListAdapter = new MyListAdapter(this, R.layout.list_contentl, arItem);
+
+        ListView myList = (ListView) findViewById(R.id.list);
+        myList.setAdapter(myListAdapter);
     }
+
+
+}
+
+class MyItem {
+    MyItem(int aimage, String aName) {
+        image = aimage;
+        name = aName;
+    }
+    int image;
+    String name;
 }
