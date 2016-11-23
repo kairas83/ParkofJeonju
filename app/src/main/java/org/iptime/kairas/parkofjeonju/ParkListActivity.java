981c49b95c5d583081bda09d8078eaa182f74595
HeyZealot <tkdlzhahffk@hanmail.net>
2016-11-23 19:31:50 +0900

    Add park
    parklistactivity에 누락된 공원 추가

--- app/src/main/java/org/iptime/kairas/parkofjeonju/ParkListActivity.java
+++ app/src/main/java/org/iptime/kairas/parkofjeonju/ParkListActivity.java
@@ -26,6 +26,29 @@ public class ParkListActivity extends AppCompatActivity {
         arItem.add(mi);
         mi = new MyItem(R.drawable.gijize, "진지제 공원");
         arItem.add(mi);
+        mi = new MyItem(R.drawable.girin_park, "기린 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.guma_park, "구마 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.hwasan_park, "화산 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.jangsung_park, "장성 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.jongsan_park, "종산 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.mansungjigu, "만성지구");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.nosong_park, "노송 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.sunsuchon_park, "선수촌 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.taepyuong_culture_park, "태평문화 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.wansan_park, "완산 공원");
+        arItem.add(mi);
+        mi = new MyItem(R.drawable.wolpyeong_park, "월평 공원");
+        arItem.add(mi);
+
 
         MyListAdapter myListAdapter = new MyListAdapter(this, R.layout.list_content, arItem);
 
