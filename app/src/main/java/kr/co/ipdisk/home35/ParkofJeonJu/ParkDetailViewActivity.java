f21cd707c8ff75a81b2e4a2d84456dc2d9872fdb
HeyZealot <tkdlzhahffk@hanmail.net>
2016-11-30 01:12:08 +0900

    ADD detail_park
    공원 상세랑 연동은 된것 같은데 사진을 어떻게 따야할지 모르것네요

--- app/src/main/java/kr/co/ipdisk/home35/ParkofJeonJu/ParkDetailViewActivity.java
+++ app/src/main/java/kr/co/ipdisk/home35/ParkofJeonJu/ParkDetailViewActivity.java
@@ -1,9 +1,17 @@
 package kr.co.ipdisk.home35.ParkofJeonJu;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by LG on 2016-11-23.
@@ -11,11 +19,70 @@ import android.view.View;
 
 public class ParkDetailViewActivity extends AppCompatActivity {
     //TODO 공원이름을 받아서 해당공원의 상세뷰를 출력해주는 클래스 만들기
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_park);
 
-    Intent intent = getIntent();
-    String name = intent.getExtras().getString("이름");
-    
 
+        Intent intent = getIntent();
+        String park_name = intent.getExtras().getString("이름");
+
+        /*
+
+        ===========================================================================
+
+         */
+
+
+        String test = "http://home35.ipdisk.co.kr/msd/SelectAllItem.php";
+        phpDown task = new phpDown(test);
+
+        task.start();
+        String result;
+
+        try {
+            task.join();
+            System.out.println("waiting... for result");
+        } catch (InterruptedException e) {
+
+        } finally {
+            result = task.getResult();
+        }
+
+        ArrayList<MyItem> arItem = new ArrayList<MyItem>();
+        MyItem mi;
+
+        try {
+            JSONObject root = new JSONObject(result);
+            JSONArray jsonArray = root.getJSONArray("results");
+            String[] name = new String[jsonArray.length()];
+            String[] parkimg_name = new String[jsonArray.length()];
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObj = jsonArray.getJSONObject(i);
+
+                name[i] = jsonObj.getString("이름");
+                parkimg_name[i] = jsonObj.getString("parkimg_name");
+            }
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                mi = new MyItem(getResources().getIdentifier(parkimg_name[i], "drawable", getPackageName()), name[i]);
+                arItem.add(mi);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        MyListAdapter myListAdapter = new MyListAdapter(this, R.layout.detail_park, arItem);
+
+        ListView myList = (ListView) findViewById(R.id.list);
+        myList.setAdapter(myListAdapter);
+
+        /*
+        =================================================================================================================
+         */
+    }
 }
 
 
